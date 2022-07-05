bd025157811dafe2efa7c54e4fae26c60c28d0a5
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-05 17:42:36 +0000

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Nicholas Fernandez';
+var isCool = 'false';
+var totalPets = 1;
+
+console.log('value of fullName', fullName);
+console.log('value of isCool', isCool);
+console.log('value of totalPets', totalPets);
+
+console.log('typeof fullName', typeof fullName);
+console.log('typeof isCool', typeof false);
+console.log('typeof totalPets', typeof 1);
+
+var unknown = 'null';
+console.log('value of unknown', unknown);
+console.log('type of unknown', typeof null);
+
+var never;
+console.log('value of never', never);
+console.log('type of never', typeof never);
