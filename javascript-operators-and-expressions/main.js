176a9dba159f6dc93c2821b8f7421001b45dd452
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-05 19:35:18 +0000

    completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,44 @@
+var width = 867530.9;
+var height = 10;
+var area = width * height;
+
+console.log('area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 500;
+var payment = 80;
+var change = bill - payment;
+
+console.log('change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 69;
+var midterm = 69;
+var final = 69;
+var grade = (quizzes * 0.25) + (midterm * 0.35) + (final * 0.40);
+
+console.log('grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Smitty';
+var lastName = 'Werbenjagermanjensen';
+var fullName = firstName + ' ' + lastName;
+
+console.log('fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 8;
+var isAcidic = (pH < 7);
+
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 300;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('type of motto:', typeof motto);
