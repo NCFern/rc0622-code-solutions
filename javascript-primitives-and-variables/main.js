eda3a807522c2bffa79fb1544db1677751c6a1f1
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-05 17:52:48 +0000

    revised code. corrected var in console logs

--- javascript-primitives-and-variables/main.js
+++ javascript-primitives-and-variables/main.js
@@ -1,14 +1,14 @@
 var fullName = 'Nicholas Fernandez';
-var isCool = 'false';
+var isCool = false;
 var totalPets = 1;
 
 console.log('value of fullName', fullName);
 console.log('value of isCool', isCool);
 console.log('value of totalPets', totalPets);
 
-console.log('typeof fullName', typeof 'Nicholas Fernandez');
-console.log('typeof isCool', typeof false);
-console.log('typeof totalPets', typeof 1);
+console.log('typeof fullName', typeof fullName);
+console.log('typeof isCool', typeof isCool);
+console.log('typeof totalPets', typeof totalPets);
 
 var unknown = 'null';
 console.log('value of unknown', unknown);
