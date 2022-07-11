52f3a16aa20896b68ed6c9af2b99a5b41fb83ff2
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-11 21:43:22 +0000

    in progress

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,1 +1,16 @@
 /* exported drop */
+
+// removes the first counts from the array and returns the rest //
+// if count > array, return array //
+
+function drop(array, count) {
+  if (count > array.length) {
+    return array;
+  }
+
+  // var finalDrop = [];
+  // for (var i = 0; i < count; i++) {
+
+  // }
+  // return finalDrop;
+}
--- array-manipulation-2/src/take.js
+++ array-manipulation-2/src/take.js
@@ -1,7 +1,7 @@
 /* exported take */
 
 // takes the count and returns the number from the array //
-// if our count is greater than the length of the array, return the arrray //
+// if our count is greater than the length of the array, return the array //
 // create an empty array //
 // loop through the array until we reach the count //
 // push the array into the final array //
