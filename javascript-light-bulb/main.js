c040ecc2fa2034027213414f320579f0a5fa2611
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-12 21:48:01 +0000

    completed javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,5 +1,7 @@
 // switch between (bulb-on + background-on) to (bulb-off + background-off) //
 // query select bulb and background so they can be changed in the function //
+// add event listener for bulb that listens for clicks //
+// make a function that when clicked, bulb/background go from on/off //
 
 var $bulb = document.querySelector('.bulb-on');
 var $background = document.querySelector('.background-on');
