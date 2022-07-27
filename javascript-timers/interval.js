a2cf86d82c8d219c4aac1359a8d7a0d0359db0d2
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-27 18:00:18 +0000

    completed javascript-timers

---
+++ javascript-timers/interval.js
@@ -0,0 +1,14 @@
+setInterval(timeCode, 1000);
+
+var getMessage = document.querySelector('.countdown-display');
+var finalCountdown = 4;
+
+function timeCode() {
+  finalCountdown--;
+  if (finalCountdown > 0) {
+    getMessage.textContent = finalCountdown;
+  } else {
+    getMessage.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(finalCountdown);
+  }
+}
