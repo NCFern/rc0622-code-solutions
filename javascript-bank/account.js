4ce646d60dc747881259b48506faab30d1c4c0c2
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-27 00:10:24 +0000

    completed javascript-bank

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -2,5 +2,39 @@
 function Account(number, holder) {
   this.number = number;
   this.holder = holder;
-
+  this.transactions = [];
 }
+
+Account.prototype.deposit = function (amount) {
+  if (typeof amount !== 'number' || amount <= 0 || !Number.isInteger(amount) || isNaN(amount)) {
+    return false;
+  }
+  var newTransaction = new Transaction('deposit', amount);
+  this.transactions.push(newTransaction);
+  return true;
+
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (typeof amount !== 'number' || amount <= 0 || !Number.isInteger(amount) || isNaN(amount)) {
+    return false;
+  }
+  var newTransaction = new Transaction('withdrawal', amount);
+  this.transactions.push(newTransaction);
+  return true;
+};
+
+Account.prototype.getBalance = function () {
+  var totalDeposits = 0;
+  var totalWithdrawals = 0;
+
+  for (var i = 0; i < this.transactions.length; i++) {
+    if (this.transactions[i].type === 'deposit') {
+      totalDeposits += this.transactions[i].amount;
+    } else {
+      totalWithdrawals += this.transactions[i].amount;
+    }
+  }
+  return totalDeposits - totalWithdrawals;
+
+};
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,34 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0 && balance % 1 === 0) {
+    var newAcccount = new Account(this.nextAccountNumber, holder);
+    newAcccount.deposit(balance);
+    this.accounts.push(newAcccount);
+    this.nextAccountNumber++;
+    return newAcccount.number;
+  } else {
+    return null;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var assets = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    assets += this.accounts[i].getBalance();
+  }
+  return assets;
+};
