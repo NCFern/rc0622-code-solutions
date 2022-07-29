b923ddc57a829ab065608b17e16ee5a05780e655
Nicholas Fernandez <Nickfer610@gmail.com>
2022-07-29 00:43:42 +0000

    completed javascript-carousel

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,33 +1,75 @@
-var $leftArrow = document.querySelector('.fa-angle-left');
-var $rightArrow = document.querySelector('.fa-angle-right');
-// var $section = document.querySelectorAll('.section');
+var musicBox = [
+  'images/OHW-IMeltWithYou.jpeg',
+  'images/OHW-HeyMickey.jpeg',
+  'images/OHW-MamboNo5.jpeg',
+  'images/OHW-MexicanRadio.jpeg',
+  'images/OHW-Mmmbop.jpeg'
+];
+var $leftArrow = document.querySelector('#left');
+var $rightArrow = document.querySelector('#right');
+var $circle = document.querySelectorAll('.fa-circle');
+var $picture = document.querySelector('.picture');
+var $circleContainer = document.querySelector('.circle-container');
 
-$leftArrow.addEventListener('click', swapLeft);
+var count = 0;
+var timer = setInterval(swapRight, 3 * 1000);
+
 $rightArrow.addEventListener('click', swapRight);
 
-function swapLeft(event) {
-
-}
-
 function swapRight(event) {
-
+  for (var i = 0; i < musicBox.length; i++) {
+    $circle[count].className = 'fa-regular fa-circle';
+    if ($picture.src.includes(musicBox[i])) {
+      $picture.setAttribute('src', musicBox[i + 1]);
+      $circle[count + 1].className = 'fa-solid fa-circle';
+      count++;
+      break;
+    } else if ($picture.src.includes(musicBox[4])) {
+      $picture.setAttribute('src', musicBox[0]);
+      count = 0;
+      $circle[count].className = 'fa-solid fa-circle';
+      break;
+    }
+  }
+  clearInterval(timer);
+  timer = setInterval(swapRight, 3 * 1000);
 }
 
-//   if (event.target.matches('.fa-angle-right')) {
-//     for (var i = 0; i < $section.length; i++) {
-//       if ($rightArrow[i] === event.target) {
-//         $section[i].className = 'section active';
-//       } else if ($section[i] !== event.target) {
-//         $section[i].className = 'section hidden';
-//       }
-//     }
-//     var $dataView = event.target.getAttribute('data-view');
-//     for (var j = 0; j < $section.length; j++) {
-//       if ($section[j].getAttribute('data-view') === $dataView) {
-//         $section[j].className = 'view';
-//       } else if ($section[j].getAttribute('data-view') !== $dataView) {
-//         $section[j].className = 'view hidden';
-//       }
-//     }
-//   }
-// }
+$leftArrow.addEventListener('click', swapLeft);
+
+function swapLeft(event) {
+  for (var i = musicBox.length; i >= 0; i--) {
+    $circle[count].className = 'fa-regular fa-circle';
+    if ($picture.src.includes(musicBox[0])) {
+      $picture.setAttribute('src', musicBox[4]);
+      $circle[4].className = 'fa-solid fa-circle';
+      count = 4;
+      break;
+    } else if ($picture.src.includes(musicBox[i])) {
+      $picture.setAttribute('src', musicBox[i - 1]);
+      $circle[count - 1].className = 'fa-solid fa-circle';
+      count--;
+      break;
+    }
+  }
+  clearInterval(timer);
+  timer = setInterval(swapRight, 3 * 1000);
+}
+
+$circleContainer.addEventListener('click', circleSelect);
+
+function circleSelect(event) {
+  if (event.target.tagName === 'I') {
+    count = parseInt(event.target.getAttribute('id'));
+    for (var i = 0; i < $circle.length; i++) {
+      $circle[i].className = 'fa-regular fa-circle';
+      if (i === parseInt(event.target.getAttribute('id'))) {
+        $circle[i].className = 'fa-solid fa-circle';
+        $picture.setAttribute('src', musicBox[i]);
+      }
+    }
+  }
+  clearInterval(timer);
+  timer = setInterval(swapRight, 3 * 1000);
+
+}
