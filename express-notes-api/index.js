afbc80ee16ffadb39aa4916dc1bca1951352c3f5
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-23 05:54:34 +0000

    in progress

---
+++ express-notes-api/index.js
@@ -0,0 +1,8 @@
+// const express = require('express');
+// const app = express();
+
+// app.use('/api/notes', express.json());
+
+// const fs = require('fs');
+
+// const data = require('./data.json');
