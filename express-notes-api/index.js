3edce78227d543c1c1dd5c021677601311538ad2
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-23 17:39:23 +0000

    in progress:

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -6,3 +6,8 @@
 // const fs = require('fs');
 
 // const data = require('./data.json');
+
+// app.listen(3000, () => {
+//   // eslint-disable-next-line no-console
+//   console.log('Express server listening on port 3000');
+// });
