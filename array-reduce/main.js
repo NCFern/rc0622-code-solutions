b7d7e6cf46968582d6a38e040aca686ebcf0d6e3
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-29 20:29:29 +0000

    in progress

---
+++ array-reduce/main.js
@@ -0,0 +1,31 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const initialSum = 0;
+const finalSum = numbers.reduce(
+  (previousValue, currentValue) => previousValue + currentValue
+);
+
+console.log(finalSum);
+
+const finalProduct = numbers.reduce(
+  (previousValue, currentValue) => previousValue * currentValue
+);
+
+console.log(finalProduct);
