d20869153f1bb10d41821ae1e0a7448ef7dc49bd
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-31 06:22:41 +0000

    completed sgt-back-end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -11,7 +11,7 @@ const db = new pg.Pool({
 const app = express();
 app.use('/api/grades', express.json());
 
-app.get('/api/grades/:gradeId', (req, res, next) => {
+app.get('/api/grades/:gradeId', (req, res) => {
   const gradeId = Number(req.params.gradeId);
 
   if (!Number.isInteger(gradeId) || gradeId < 1) {
@@ -53,7 +53,7 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
     });
 });
 
-app.get('/api/grades', (req, res, next) => {
+app.get('/api/grades', (req, res) => {
   const sql = `
   select "gradeId",
          "name",
@@ -75,18 +75,118 @@ app.get('/api/grades', (req, res, next) => {
     });
 });
 
-app.post('/api/grades', (req, res, next) => {
+app.post('/api/grades', (req, res) => {
   const entry = req.body;
   if (!entry.name || !entry.course) {
     res.status(400).json({
-      error: 'Please entry valid entry'
+      error: 'Please enter valid name and/or course'
     });
+  } else if (!Number.isInteger(Number(entry.score)) || entry.score <= 0) {
+    res.status(400).json({
+      error: 'Please enter valid score'
+    });
+  } else {
+    const sql = `
+  insert into "grades" ("name", "course", "score")
+  values ($1,$2,$3)
+  returning *
+  `;
+    const params = [entry.name, entry.course, entry.score];
+    db.query(sql, params)
+      .then(result => {
+        res.status(201).json(entry);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          error: 'An unexpected error occured'
+        });
+      });
   }
-  // const sql = `
-  // insert into "grades" ("name", "course", "score")
-  // values ($1,$2,$3)
-  // returning *
-  // `;
+});
+
+app.put('/api/grades/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const entry = req.body;
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).send({
+      error: 'id must be a positive integer'
+    });
+    return;
+  }
+  if (!entry.name || !entry.course) {
+    res.status(400).json({
+      error: 'Please enter valid name and/or course'
+    });
+  } else if (!Number.isInteger(Number(entry.score)) || entry.score <= 0) {
+    res.status(400).json({
+      error: 'Please enter valid score'
+    });
+  } else {
+    const sql = `
+    update "grades"
+    set "name" = $1,
+        "course" = $2,
+        "score" = $3
+        where "gradeId" = $4
+        returning *
+        `;
+    const params = [entry.name, entry.course, entry.score, id];
+    db.query(sql, params)
+      .then(result => {
+        if (!result.rows[0]) {
+          res.status(404).json({
+            error: `Cannot find a record matching ${id}`
+          });
+        }
+        res.status(200).json(entry);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({
+          error: 'An unexpected error occured'
+        });
+      });
+  }
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (!Number.isInteger(id) || id <= 0) {
+    res.status(400).json({
+      error: 'id must be a positive interger'
+    });
+    return;
+  }
+  if (!id) {
+    res.status(404).json({
+      error: 'An unexpected error occured'
+    });
+    return;
+  }
+  const sql = `
+  delete from "grades"
+  where "gradeId" = $1
+  returning *
+  `;
+  const params = [id];
+  db.query(sql, params)
+    .then(result => {
+      if (!result.rows[0]) {
+        res.status(404).json({
+          error: `Cannot find a record matching ${id}`
+        });
+      }
+      res.status(204).json({
+        success: 'deleted entry'
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occured'
+      });
+    });
 });
 
 app.listen(3000, () => {
