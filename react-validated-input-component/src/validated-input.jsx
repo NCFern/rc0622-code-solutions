c4c543b07ca29700de32a473f2832c13a6bcc2b4
Nicholas Fernandez <Nickfer610@gmail.com>
2022-09-06 18:42:19 +0000

    in progress

---
+++ react-validated-input-component/src/validated-input.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class HotButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handlePassword = this.handlePassword.bind(this);
+  }
+
+  handlePassword() {
+
+  }
+
+  // render() {
+
+  // }
+}
