aa5dc34d324cee1ea7ec45a14613dbf170d6af54
Nicholas Fernandez <Nickfer610@gmail.com>
2022-09-06 20:31:23 +0000

    in progress

--- react-validated-input-component/src/index.jsx
+++ react-validated-input-component/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ValidatedInput from './validated-input';
+import ValidatedInput from './validated-input.jsx';
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -1,17 +1,55 @@
 import React from 'react';
 
-export default class HotButton extends React.Component {
+export default class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value: '',
+      errorMessage: ''
+    };
     this.handlePassword = this.handlePassword.bind(this);
   }
 
-  handlePassword() {
-
+  handlePassword(event) {
+    this.setState({ value: event.target.value });
+    // if no password, show red x //
+    // message- a password is required //
+    if (event.target.value === '') {
+      this.setState({
+        errorMessage: 'a password is required'
+      });
+    // if password is less than 8 charas, show red x //
+    // message- password is too short //
+    } else if (event.target.value.length < 8) {
+      this.setState({
+        errorMessage: 'password is too short'
+      });
+    } else {
+      this.setState({
+        errorMessage: null
+      });
+    }
   }
 
-  // render() {
+  render() {
 
-  // }
+    return (
+        <form>
+      <div>
+        <label htmlFor='input-password'>Password</label>
+      </div>
+      <input
+      type="password"
+      id='input-password'
+      value={this.state.value}
+      onChange={this.handlePassword}>
+      </input>
+      <div className='error-message'>
+        <span className='error-text'>{this.state.errorMessage}</span>
+      </div>
+
+    </form>
+    );
+
+  }
 }
