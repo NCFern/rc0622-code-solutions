1e94e6cc44d37efe662b90f2a05bbb6746a74c16
Nicholas Fernandez <Nickfer610@gmail.com>
2022-09-09 22:23:22 +0000

    in progress

---
+++ react-accordion-component/src/Satisfaction.jsx
@@ -0,0 +1,6 @@
+export const SatisfactionArray = [
+  { name: 'Crow Hogan', text: 'You know what they say about birds of a feather...' },
+  { name: 'Kiryu Kyosuke', text: 'With this.... Im satisfied.' },
+  { name: 'Jack Atlas', text: 'This is my BURNING SOUL!' },
+  { name: 'Yusei Fudo', text: 'The future isnt written yet. There is still time for things to change.' }
+];
